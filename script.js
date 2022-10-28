43efcaac197105bca83aeed1fec6f6ff10912088
MaddoxHMS <adam.maddox@bisdmail.net>
2022-10-28 15:59:04 -0500

    Add operations buttons via JS

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const nButtonArea = document.querySelector(".numbers")
+const opButtonArea = document.querySelector(".operations")
 
 let nButtons = [7,8,9,4,5,6,1,2,3,0,"."];
-let opButtons = ["+","-","*","/","Clear","Enter"]
+let opButtons = ["CLEAR","+","-","*","/","ENTER"]
 
 nButtons.forEach(button=>{
     let newButton = document.createElement("div");
@@ -9,4 +10,11 @@ nButtons.forEach(button=>{
     newButton.classList.add(`numb${button}`);
     nButtonArea.appendChild(newButton);
 
+});
+
+opButtons.forEach(button=>{
+    let newButton = document.createElement("div");
+    newButton.textContent = button;
+    newButton.classList.add(`op${button}`);
+    opButtonArea.appendChild(newButton);
 });
